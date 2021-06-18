0d2c55d293affafed69238118ca2849b45b36961
Piyush Tank <piyush13290@gmail.com>
2021-06-18 16:12:00 -0500

    added almost all basic functionality

--- PyList.py
+++ PyList.py
@@ -42,10 +42,7 @@ class PyList:
 
         self.items[self.numItems] = item
         self.numItems += 1
-        
-  
-
-
+    
     def insert(self, i, newitem):
         '''
         To insert an item in the list
@@ -79,10 +76,104 @@ class PyList:
 
         raise IndexError("Index is out of range for PyList")
 
+    def __add__(self,other):
+        '''
+        to add another PyList with this 
+        Input:
+            other = PyList object
+        Output
+            result = PyList object adding the existing list with the other list 
+        '''
+        if type(self) != type(other):
+            raise TypeError("the object does not seems to be of PyList type, it is {} type".format(type(other)))
+            print("this is still running")
 
+        newsize = int((self.numItems + other.numItems)*1.5)
+        result = PyList(size = newsize) # to make sure new PyList if of adequet size
+
+        for i in range(self.numItems):
+            result.append(i)
+
+        for i in range(other.numItems):
+            result.append(i)
+
+        return result
+
+    
+    def __delitem__(self, index):
+        '''
+        to delete an item from the PyList
+        '''
+
+        if index > self.numItems-1:
+            raise ValueError("Index should be lower, the list does not have item in this index")
+
+        for i in range(index, self.numItems-1):
+            self.items[i] = self.items[i+1]
+        
+        self.numItems -= 1 
+
+    def __eq__(self, other):
+        '''
+        To check if two PyList are same or not
+        '''
+        #check 1 : if type is not similar 
+        if type(self) != type(other):
+            return False
+        
+        #check 2: if num of items in both lists are not similar 
+        if self.numItems != other.numItems:
+            return False
+        
+        #check 3: now checking each element of both lists, break it as soon as one of the items is not similar
+        for i in range(self.items):
+            if self.items[i] != other.items[i]:
+                return False
+        
+        # check 4: okay, if you have reached so far, they must be similar lists
+        return True
+
+    def __iter__(self):
+        '''
+        a method to iterate over the list 
+        '''
+        for i in range(self.numItems):
+            return self.items[i]
+
+    def __len__(self):
+        '''
+        to get the length of the PyList object 
+        '''
+        return self.numItems
+
+    def __contains__(self, item):
+        '''
+        to see if the PyList contains this item; linear search in the PyList
+        '''
+        for i in range(self.numItems):
+            if self.items[i] == item:
+                return True
+        
+        return False
+
+    def __repr__(self):
+        '''
+        repr method to see the object more meaningfully
+        '''
+        s = "PyList(["
+        for i in range(self.numItems):
+            s += repr(self.items[i])
+            if i < self.numItems-1:
+                s += ", "
+        s += "])"
+        return s
 
 if __name__=='__main__':
     example = PyList(['a','b','c','d'], size=3)
+    example2 = PyList([1,2,3], 5)
+    print(example)
+
+
 
 
     
